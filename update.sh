c742baec293efe1de08f8b19741ebdda0519b0ee
Joshua Clements <josh@penpow.dev>
2022-08-01 18:07:53 +0100

    refactor(update.sh): final fixes
    omg this works

--- update.sh
+++ update.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
 
+if [[ -z "$GH_PAT" ]]; then
+	echo "Run this with the GH PAT env variable set"
+	exit 1
+fi
+
 # lil helper script
 compare_versions() {
     [  "$1" = "`echo -e "$1\n$2" | sort -V | head -n1`" ]
@@ -15,11 +20,9 @@ else
 	VERSION=$(cat $VERSION_FILE)
 fi
 
-echo $VERSION
-
 REQUEST=$(curl -s -H "Accept: application/vnd.github+json"  -H "Authorization: token ${GH_PAT}" "https://api.github.com/repos/PenPow/Sentry/releases/latest")
 
-NEW_VERSION=$(jq -r 'if .draft or .prerelease then "999.999.999" else if .message? == "Not Found" then "0.0.0" else .tag_name end end' <<< $REQUEST)
+NEW_VERSION=$(jq -r 'if .draft or .prerelease then "999.999.999" else if .message? == "Not Found" or .message? == "Bad Credentials" then "0.0.0" else .tag_name end end' <<< $REQUEST)
 
 UPDATE=$(compare_versions ${NEW_VERSION} ${VERSION}) && true || false
 
@@ -27,7 +30,7 @@ if [[ UPDATE ]] && [[ $VERSION != $NEW_VERSION ]]; then
 	# Time to update
 
 	echo Updating from $VERSION to $NEW_VERSION
-	docker compose pull -f docker-compose.production.yaml && docker compose up -d -f docker-compose.production.yaml
+	docker compose -f docker-compose.production.yaml pull && docker compose -f docker-compose.production.yaml up -d
 
 	echo $NEW_VERSION > "$VERSION_FILE"
 else
