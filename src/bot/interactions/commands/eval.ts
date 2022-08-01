dccfb554175d2ea4756261e85ec23670a4ef2b6c
Joshua Clements <josh@penpow.dev>
2022-08-01 20:08:32 +0100

    refactor(eval): misc eval changes and fixes

--- src/bot/interactions/commands/eval.ts
+++ src/bot/interactions/commands/eval.ts
@@ -1,12 +1,11 @@
 import { inspect } from "util";
-import { ApplicationCommandOptionType, ApplicationCommandType, codeBlock, EmbedBuilder } from "discord.js";
+import { ActionRowBuilder, ApplicationCommandOptionType, ApplicationCommandType, ButtonBuilder, ButtonStyle, codeBlock, EmbedBuilder } from "discord.js";
 import { nanoid } from "nanoid";
 import { get, create, SourceBin } from 'sourcebin';
 import pkg from "typescript";
 import { translate } from "../../../common/translations/translate.js";
 import { InteractionManager, ResponseType } from "../../managers/InteractionManager.js";
 import { FunctionType, IFunction, PermissionTier } from "../../structures/Interaction.js";
-// import { log, LogLevel } from "../../../common/logger.js";
 
 const { transpile, getParsedCommandLineOfConfigFile, sys, ModuleKind, ModuleResolutionKind, ScriptTarget } = pkg;
 
@@ -20,12 +19,15 @@ const EvalCommand: IFunction = {
 
 		let code = bin.files[0].content.replaceAll(/[“”]/gim, '"');
 
-		if (code.includes('return')) code = `(() => { \n${code} })()`;
-		if (code.includes('await')) code = `(async () => { \n${bin.files[0].content.replaceAll(/[“”]/gim, '"')} })()`;
+		if (code.includes('await')) code = `(async () => { \n${code} })()`;
+		else if (code.includes('return')) code = `(() => { \n${code} })()`;
 
 		code = code.replaceAll('client', "interaction.client");
 
 		const embed = new EmbedBuilder();
+
+		let extendedURL: SourceBin | null = null;
+
 
 		if (bin.files[0].languageId === 183) {
 			try {
@@ -33,9 +35,7 @@ const EvalCommand: IFunction = {
 				const evaled = await eval(code);
 				const inspected = inspect(evaled, { depth: 5, maxArrayLength: 5, getters: true });
 
-				let extendedURL: SourceBin | null = null;
-
-				if (inspected.length > 1015) extendedURL = await create([{ content: inspected, language: 'js' }], { title: `Output Log (${nanoid()})` }).catch();
+				if (inspected.length > 900) extendedURL = await create([{ content: inspected, language: 'js' }], { title: `Output Log (${nanoid()})` }).catch();
 
 				embed
 					.setTimestamp()
@@ -44,13 +44,9 @@ const EvalCommand: IFunction = {
 					.setAuthor({ iconURL: interaction.user.displayAvatarURL(), name: interaction.user.tag })
 					// eslint-disable-next-line @typescript-eslint/restrict-template-expressions, @typescript-eslint/no-unsafe-member-access
 					.setDescription([`**Time Taken**: ${(Date.now() - interaction.createdTimestamp).toLocaleString()}ms`, `**Output Type**: ${evaled?.constructor.name === "Array" ? `${evaled.constructor.name}<${evaled[0]?.constructor.name}>` : evaled?.constructor.name ?? typeof evaled}`, `**Output Length**: ${inspected.length} characters`].join('\n'))
-					.addFields(...[{ name: 'Input', value: codeBlock('js', code.substring(0, 1015)) }, { name: 'Output', value: extendedURL ? `${extendedURL.url}\n${codeBlock("js", inspected.substring(0, 984))}` : codeBlock("js", inspected.substring(0, 1015)) }]);
+					.addFields(...[{ name: 'Input', value: codeBlock('js', code.substring(0, 900)) }, { name: 'Output', value: codeBlock("js", inspected.substring(0, 900)) }]);
 			} catch (e) {
-				// log({ level: LogLevel.Error, prefix: 'Eval Command' }, inspect(e, { depth: 100, colors: true }));
-
-				let extendedURL: SourceBin | null = null;
-
-				if (((e as Error).stack?.length ?? 0) > 1015) extendedURL = await create([{ content: (e as Error).stack ?? 'An Error Occured Making this Error', language: 'js' }], { title: `Error Log (${nanoid()})` }).catch();
+				if (((e as Error).stack?.length ?? 0) > 900) extendedURL = await create([{ content: (e as Error).stack ?? 'An Error Occured Making this Error', language: 'js' }], { title: `Error Log (${nanoid()})` }).catch();
 
 				embed
 					.setTimestamp()
@@ -59,7 +55,7 @@ const EvalCommand: IFunction = {
 					.setAuthor({ iconURL: interaction.user.displayAvatarURL(), name: interaction.user.tag })
 					// eslint-disable-next-line @typescript-eslint/restrict-template-expressions, @typescript-eslint/no-unsafe-member-access
 					.setDescription([`**Time Taken**: ${(Date.now() - interaction.createdTimestamp).toLocaleString()}ms`, `**Output Type**: ${(e as Error).name}`, `**Output Length**: ${(e as Error).stack?.length}`].join('\n'))
-					.addFields(...[{ name: 'Input', value: codeBlock('ts', code.substring(0, 1015)) }, { name: 'Error', value: extendedURL ? `${extendedURL.url}\n${codeBlock("js", (e as Error).stack?.substring(0, 984) ?? (e as Error).message)}` : codeBlock("js", (e as Error).stack?.substring(0, 1015) ?? (e as Error).message) }]);
+					.addFields(...[{ name: 'Input', value: codeBlock('ts', code.substring(0, 900)) }, { name: 'Error', value: codeBlock("js", (e as Error).stack?.substring(0, 900) ?? (e as Error).message) }]);
 			}
 		} else if (bin.files[0].languageId === 378) {
 			const options = getParsedCommandLineOfConfigFile(
@@ -86,9 +82,7 @@ const EvalCommand: IFunction = {
 				const evaled = await eval(transpiled);
 				const inspected = inspect(evaled, { depth: 5, maxArrayLength: 5, getters: true });
 
-				let extendedURL: SourceBin | null = null;
-
-				if (inspected.length > 1015) extendedURL = await create([{ content: inspected, language: 'js' }], { title: `Output Log (${nanoid()})` }).catch();
+				if (inspected.length > 900) extendedURL = await create([{ content: inspected, language: 'js' }], { title: `Output Log (${nanoid()})` }).catch();
 
 				embed
 					.setTimestamp()
@@ -97,13 +91,9 @@ const EvalCommand: IFunction = {
 					.setAuthor({ iconURL: interaction.user.displayAvatarURL(), name: interaction.user.tag })
 					// eslint-disable-next-line @typescript-eslint/restrict-template-expressions, @typescript-eslint/no-unsafe-member-access
 					.setDescription([`**Time Taken**: ${(Date.now() - interaction.createdTimestamp).toLocaleString()}ms`, `**Output Type**: ${evaled?.constructor.name === "Array" ? `${evaled.constructor.name}<${evaled[0]?.constructor.name}>` : evaled?.constructor.name ?? typeof evaled}`, `**Output Length**: ${inspected.length} characters`].join('\n'))
-					.addFields(...[{ name: 'Input', value: codeBlock('ts', code.substring(0, 1015)) }, { name: 'Transpiled', value: codeBlock('js', transpiled.substring(0, 1015)) }, { name: 'Output', value: extendedURL ? `${extendedURL.url}\n${codeBlock("js", inspected.substring(0, 984))}` : codeBlock("js", inspected.substring(0, 1015)) }]);
+					.addFields(...[{ name: 'Input', value: codeBlock('ts', code.substring(0, 900)) }, { name: 'Transpiled', value: codeBlock('js', transpiled.substring(0, 900)) }, { name: 'Output', value: codeBlock("js", inspected.substring(0, 900)) }]);
 			} catch (e) {
-				// log({ level: LogLevel.Error, prefix: 'Eval Command' }, inspect(e, { depth: 100, colors: true }));
-
-				let extendedURL: SourceBin | null = null;
-
-				if (((e as Error).stack?.length ?? 0) > 1015) extendedURL = await create([{ content: (e as Error).stack ?? 'An Error Occured Making this Error', language: 'js' }], { title: `Error Log (${nanoid()})` }).catch();
+				if (((e as Error).stack?.length ?? 0) > 900) extendedURL = await create([{ content: (e as Error).stack ?? 'An Error Occured Making this Error', language: 'js' }], { title: `Error Log (${nanoid()})` }).catch();
 
 				embed
 					.setTimestamp()
@@ -112,7 +102,7 @@ const EvalCommand: IFunction = {
 					.setAuthor({ iconURL: interaction.user.displayAvatarURL(), name: interaction.user.tag })
 					// eslint-disable-next-line @typescript-eslint/restrict-template-expressions, @typescript-eslint/no-unsafe-member-access
 					.setDescription([`**Time Taken**: ${(Date.now() - interaction.createdTimestamp).toLocaleString()}ms`, `**Output Type**: ${(e as Error).name}`, `**Output Length**: ${(e as Error).stack?.length}`].join('\n'))
-					.addFields(...[{ name: 'Input', value: codeBlock('ts', code.substring(0, 1015)) }, { name: 'Transpiled', value: codeBlock('js', transpiled.substring(0, 1015)) }, { name: 'Error', value: extendedURL ? `${extendedURL.url}\n${codeBlock("js", (e as Error).stack?.substring(0, 984) ?? (e as Error).message)}` : codeBlock("js", (e as Error).stack?.substring(0, 1015) ?? (e as Error).message) }]);
+					.addFields(...[{ name: 'Input', value: codeBlock('ts', code.substring(0, 900)) }, { name: 'Transpiled', value: codeBlock('js', transpiled.substring(0, 900)) }, { name: 'Error', value: codeBlock("js", (e as Error).stack?.substring(0, 900) ?? (e as Error).message) }]);
 			}
 		} else {
 			embed
@@ -124,7 +114,18 @@ const EvalCommand: IFunction = {
 				.setAuthor({ name: interaction.user.tag, iconURL: interaction.user.displayAvatarURL() });
 		}
 
-		return void await InteractionManager.sendInteractionResponse(interaction, { ephemeral: true, embeds: [embed] }, ResponseType.FollowUp);
+		const row = new ActionRowBuilder<ButtonBuilder>().addComponents(...[new ButtonBuilder().setStyle(ButtonStyle.Link).setEmoji("🔗")
+			.setLabel("Source Code")
+			.setURL(bin.url), extendedURL
+			? new ButtonBuilder().setStyle(ButtonStyle.Link).setEmoji("📁")
+				.setLabel("Output")
+				.setURL(extendedURL.url)
+			: new ButtonBuilder().setStyle(ButtonStyle.Secondary).setEmoji("📁")
+				.setLabel("Output")
+				.setCustomId('noop')
+				.setDisabled(true)]);
+
+		return void await InteractionManager.sendInteractionResponse(interaction, { ephemeral: true, embeds: [embed], components: [row] }, ResponseType.FollowUp);
 	},
 	toJSON() {
 		return {
