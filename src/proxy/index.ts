1b24500d58e5477fe4b71815952e1a3bd7d86cc5
PenPow <josh@penpow.dev>
2022-11-20 08:22:38 +0000

    fix(rest): fix starting on unintentional port

--- src/proxy/index.ts
+++ src/proxy/index.ts
@@ -8,5 +8,5 @@ import { config, logger } from '../common/utils.js';
 const api = new REST({ rejectOnRateLimit: () => true, retries: 0 }).setToken(config.discord.TOKEN);
 const server = createServer(proxyRequests(api));
 
-const port = Number.parseInt(config.proxy.PORT ?? '8080', 10);
+const port = Number.parseInt(config.proxy.PORT, 10);
 server.listen(port, () => logger.info(`Listening on port ${port}`));
